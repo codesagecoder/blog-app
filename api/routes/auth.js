8f9d30db5ff4ff7d3fafc9954b5090b726318042
codesage <laughingsagecoder@gmail.com>
2023-12-29 18:49:34 +0200

    changing password encryption/decryption key

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -9,7 +9,7 @@ router.post("/register", async (req, res) => {
     try {
         const passwordEncrypt = CryptoJS.AES.encrypt(
             req.body.password,
-            process.env.SECRET_KEY
+            process.env.AES_KEY
         ).toString()
         const newUser = new User({ ...req.body, password: passwordEncrypt });
         const user = await newUser.save();
@@ -25,7 +25,7 @@ router.post("/login", async (req, res) => {
         const user = await User.findOne({ username: req.body.username });
         !user && res.status(401).json("Wrong password or username!");
 
-        const passWordArray = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const passWordArray = CryptoJS.AES.decrypt(user.password, process.env.AES_KEY);
         const originalPassword = passWordArray.toString(CryptoJS.enc.Utf8);
 
         if (originalPassword !== req.body.password) {
